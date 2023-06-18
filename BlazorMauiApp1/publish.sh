6e176db9ffb144f15c54fdcc1f4f60b41022d768
Gaël James <gael.james@aisin-europe.com>
2023-06-18 18:16:56 +0200

    🚚 Move keystore into the project folder

--- BlazorMauiApp1/publish.sh
+++ BlazorMauiApp1/publish.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-dotnet publish -f net7.0-android33.0 -c Release -p:AndroidSigningKeyAlias=mauiblazor -p:AndroidSigningKeyStore=../MauiBlazor.keystore -p:AndroidKeyStore=true -p:AndroidSigningKeyPass=file:../AndroidSigningPassword.txt -p:AndroidSigningStorePass=file:../AndroidSigningPassword.txt
+rm -rf bin
+dotnet publish -f net7.0-android33.0 -c Release -p:AndroidSigningKeyAlias=mauiblazor -p:AndroidSigningKeyStore=MauiBlazor.keystore -p:AndroidKeyStore=true -p:AndroidSigningKeyPass=file:AndroidSigningPassword.txt -p:AndroidSigningStorePass=file:AndroidSigningPassword.txt
