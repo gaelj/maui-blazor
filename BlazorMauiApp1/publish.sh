b3dfb8ca785fd9f7f963756faf63c8f6cf460ea9
Gaël James <gael.james@aisin-europe.com>
2023-06-18 18:36:26 +0200

    🔧 Publish to publish folder, add iOS

--- BlazorMauiApp1/publish.sh
+++ BlazorMauiApp1/publish.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 rm -rf bin
-dotnet publish -f net7.0-android33.0 -c Release -p:AndroidSigningKeyAlias=mauiblazor -p:AndroidSigningKeyStore=MauiBlazor.keystore -p:AndroidKeyStore=true -p:AndroidSigningKeyPass=file:AndroidSigningPassword.txt -p:AndroidSigningStorePass=file:AndroidSigningPassword.txt
+rm -rf publish
+dotnet publish -f net7.0-android33.0 -r android-arm64 --self-contained true -c Release -p:AndroidSigningKeyAlias=mauiblazor -p:AndroidSigningKeyStore=MauiBlazor.keystore -p:AndroidKeyStore=true -p:AndroidSigningKeyPass=file:AndroidSigningPassword.txt -p:AndroidSigningStorePass=file:AndroidSigningPassword.txt --output ./publish/android/
+dotnet publish -f net7.0-ios -r ios-arm64 --self-contained true -c Release --output ./publish/ios/
